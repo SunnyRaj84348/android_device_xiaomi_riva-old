3665c4e9a879da177d4d568370c7ff69f49aa2fa
muralivijay <muralidharan9845@gmail.com>
2017-11-05 14:37:48 +0530

    revert androidmake for build fix

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,5 +26,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/rolex/rolex:6.0.1/MMB29M/V8.5.1.0.MCCMIED:user/release-keys \
-    PRIVATE_BUILD_DESC="rolex-user 6.0.1 MMB29M V8.5.1.0.MCCMIED release-keys"
+PRIVATE_BUILD_DESC="rolex-user 6.0.1 MMB29M V8.5.1.0.MCCMIED release-keys"
 
