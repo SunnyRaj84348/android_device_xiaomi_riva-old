1e917e5ea79432646f0ef4e4ddb174c5163a0b60
muralivijay <muralidharan9845@gmail.com>
2017-12-07 14:39:46 +0530

    disable camera into media

--- device.mk
+++ device.mk
@@ -221,11 +221,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8937 \
-    Snap
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
