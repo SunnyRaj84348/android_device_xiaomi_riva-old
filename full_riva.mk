06cd28b13fcb958913fd0fb556850fa53288ad3f
muralivijay <muralidharan9845@gmail.com>
2018-01-30 14:43:16 +0530

    changed device code names

--- full_riva.mk
+++ full_riva.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,13 +18,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from rolex device
-$(call inherit-product, device/xiaomi/rolex/device.mk)
+# Inherit from riva device
+$(call inherit-product, device/xiaomi/riva/device.mk)
 
 # Device identifier. This must come after all inclusions
 TARGET_VENDOR := Xiaomi
-PRODUCT_DEVICE := rolex
-PRODUCT_NAME := full_rolex
+PRODUCT_DEVICE := riva
+PRODUCT_NAME := full_riva
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 4A
+PRODUCT_MODEL := Redmi 5A
 PRODUCT_MANUFACTURER := Xiaomi
