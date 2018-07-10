8c6c4d96d6c188382d8197dd0a2f28e5f57284dc
Danny Baumann <dannybaumann@web.de>
2018-07-10 09:09:29 +0200

    riva: Update call recording overlay.
    Change-Id: I3428fa6e47318b195a8dc46731e0aabc14a9a8e7

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/xiaomi/riva/riva-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     Bluetooth \
