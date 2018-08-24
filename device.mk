22f6560ed612b5df4d306297066c75096e117b1a
muralivijay <muralidharan9845@gmail.com>
2018-08-24 19:15:00 +0530

    riva: Drop devicesettings and Doze
    This is clone duplicate settings on crdroid and other lineage based roms .
    
    Revert "rolex: Doze: Minor code simplification and clean up"
    
    This reverts commit 4401b0e37a8bedf793ec39756d7358f1bbb2c8b0.
    
    Revert "rolex: Import xiaomidoze from xiaomi/msm8996"
    
    This reverts commit c3cbbe5f43ee31d0faeb61cf54dfac6007adb409.

--- device.mk
+++ device.mk
@@ -197,10 +197,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service 
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
@@ -222,10 +218,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
-
-# Gestures and Glove mode package
-PRODUCT_PACKAGES += \
-    DeviceSettings
 
 # GPS
 PRODUCT_PACKAGES += \
