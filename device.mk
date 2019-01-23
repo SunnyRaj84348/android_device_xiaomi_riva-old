rajkirananr/android_device_xiaomi_riva-1

riva: Don't build lineage vibrator hal

device.mk
@@ -393,7 +393,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \

SunnyRaj84348/android_device_xiaomi_riva-old

device.mk
@@ -393,7 +393,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \
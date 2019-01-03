1a05e1f26e22bb462122cb308a9f69c895525a44
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-01-03 13:37:29 +0530

    riva: Don't build lineage vibrator hal

--- device.mk
+++ device.mk
@@ -393,7 +393,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \
